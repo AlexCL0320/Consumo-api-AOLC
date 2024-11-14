20a9f2a816dbb033fd888111ed4aa0d411009310
AlexCL0320 <lopez.oswaldo.1d@gmail.com>
2024-11-14 09:40:03 -0600

    Consumo API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), NgxPaginationModule], 
+  bootstrap: [AppRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { UserService } from './services/user.service';
+import { UserListComponent } from './user-list/user-list.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+UserListComponent
+],
+imports: [
+BrowserModule,
+HttpClientModule, NgxPaginationModule
+],
+providers: [UserService],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
